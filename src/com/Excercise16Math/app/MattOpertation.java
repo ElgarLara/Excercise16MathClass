c97009e60f2621e11c085fa9bda6f94eb4490886
akain88 <larahaze88@gmail.com>
2019-10-21 13:32:41 -0500

    use of math. where the most basic oerations are seeing

---
+++ src/com/Excercise16Math/app/MattOpertation.java
@@ -0,0 +1,19 @@
+package com.Excercise16Math.app;
+import java.lang.Math;
+public class MattOpertation {
+
+	public static void main(String[] args) 
+	{
+		double number= 3.5;
+		System.out.println("e value:"+Math.E);
+		System.out.println("PI value:"+Math.PI);
+		System.out.println("Absolute value of +2.14 is"+Math.abs(-2.15));
+		System.out.println("The cube Root od 27 is "+Math.cbrt(27));
+		System.out.println("The round of 3.27 "+Math.round(number));
+		System.out.println("The ceil of 3.27 "+Math.ceil(number));
+		System.out.println("The floor of 3.27 "+Math.floor(number));
+		
+
+	}
+
+}
